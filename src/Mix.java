ca1b6d1015726c504ce3ad4c3377529ca2107f1d
Xabi <xlazcanoit18dw@ikzubirimanteo.com>
2018-10-23 12:23:59 +0200

    a

--- src/Mix.java
+++ src/Mix.java
@@ -9,6 +9,13 @@ public class Mix {
 		while (flag==0) {
 		
 		System.out.println("Choose what exercise do you want me to do between 1 and 7 (0 to quit): ");
+		System.out.println("1 Given a positive number tells how many even digits has the number");
+		System.out.println("2 Given a positive number and a digit tells whether the digit appears in the number or not and in case yes, how many times it does");
+		System.out.println("3 Given a positive number it gives us the inverted number");
+		System.out.println("4 Given a binary number it gives us its decimal value");
+		System.out.println("5 Given a sentence and a letter it gives the number of words of the sentence that begin with that letter");
+		System.out.println("6 Given a sentence and a letter it gives the number of words of the sentence that end with that letter");
+		System.out.println("7 Given a word it tells the user whether the word is a palindrome or it is not");
 		int a = sc.nextInt();
 		
 		switch (a) {
@@ -35,10 +42,7 @@ public class Mix {
 			}
 			
 				System.out.println("The number " + b + " has "+counter+" even numbers");
-				
-			
-				
-				
+				System.out.println();	
 			
 			}
 			break;
@@ -47,26 +51,21 @@ public class Mix {
 		
 			System.out.println("Enter a positive number:");
 			
-			String ñ = sc.next();
-			int j = Integer.parseInt(ñ);
+			String ñ = sc.next(); //we receive a string for using length function
+			int j = Integer.parseInt(ñ); //converts a string into an integer
 			
 			System.out.println("Enter a digit");
-			String d = sc.next();
-			int y = Integer.parseInt(d);
-		
+			char d = sc.next().charAt(0); //we receive a digit	
 			
-			int c = ñ.length();
-			int f = d.length();
-			char[] arraia = ñ.toCharArray();
-			char digit = d.charAt(0);
+			int c = ñ.length(); //we save length of the string
+			
+			char[] arraia = ñ.toCharArray(); //we create an array with the given number
 			
 			int counter=0;
-			if (j>0 && f==1 && y>0) {
-			for (int i=0;i<c;i++) {
+			if (j>0 && d>0) { //it verifies that the number and the digit are positive (the ASCII value of the chars)
+			for (int i=0;i<c;i++) { // it goes on from cell to cell
 				
-				//System.out.println(d);
-		
-			if (arraia[i]==digit) {
+			if (arraia[i]==d) { //if both chars are the same it sums 1 to the counter
 				counter++;
 			}
 			
@@ -80,15 +79,15 @@ public class Mix {
 			break;
 		case 3:
 			System.out.println("Give me a number and I will give you its inverted one");
-			String case3 = sc.next();
-		    int luz = case3.length();
-		    char[] ariketa = case3.toCharArray();
+			String case3 = sc.next(); //we receive a string for using length function
+		    int luz = case3.length(); //we save the length of the string
+		    char[] ariketa = case3.toCharArray(); //we create a char array with the number
 		    
-			System.out.println("your number is " + case3);
-			System.out.println("The inverted number is");
+			System.out.println("Your number is " + case3); 
+			System.out.print("The inverted number is ");
 		    
-		    for (int i=luz-1;i>-1;i--) {
-		    	System.out.print(ariketa[i]);
+		    for (int i=luz-1;i>-1;i--) { //it creates a loop starting from the last cell of the array and finishing in the first one
+		    	System.out.print(ariketa[i]); //it prints the chars in the same line
 		    	
 		    }
 		    System.out.println();
